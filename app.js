7b5a37290b2b0fe78d425f4fea989baa5563c364
Kevin Vicrey <kevin.vicrey@nexu.org>
2012-11-10 22:14:30 -0500

    Remove routes server-side since it is a SPA.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -28,8 +26,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){ res.render('index') });
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
